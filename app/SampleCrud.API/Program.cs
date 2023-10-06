1de255941f9c1b7830355ab1dbcd157eb1ccb207
guigarciaa <guigarcia.a@outlook.com>
2023-10-06 18:33:30 -0300

    Create docker file and create recurses in docker-compose, inject db in app.

--- app/SampleCrud.API/Program.cs
+++ app/SampleCrud.API/Program.cs
@@ -1,11 +1,13 @@
+using SampleCrud.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfraDatabase(builder.Configuration);
 
 var app = builder.Build();
 
