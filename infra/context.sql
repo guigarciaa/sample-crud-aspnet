75d422d130686753938c0051c2853c3b8af43807
guigarciaa <guigarcia.a@outlook.com>
2023-10-14 17:38:03 -0300

    add script sql

---
+++ infra/context.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "Person" (
+          "Id" uuid NOT NULL,
+          "Nickname" character varying(32) NOT NULL,
+          "Name" character varying(100) NOT NULL,
+          "Email" text NOT NULL,
+          "Birthday" date NOT NULL,
+          "Stack" text[] NULL,
+          CONSTRAINT "PK_Person" PRIMARY KEY ("Id")
+      );
