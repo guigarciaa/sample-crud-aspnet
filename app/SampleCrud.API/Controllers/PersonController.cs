054d22104518f6feb56f520f2ef936503f647d06
guigarciaa <guigarcia.a@outlook.com>
2023-10-18 00:57:38 -0300

    removing reference unusable

--- app/SampleCrud.API/Controllers/PersonController.cs
+++ app/SampleCrud.API/Controllers/PersonController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SampleCrud.Domain.Entities;
 using SampleCrud.Domain.Services;
 
