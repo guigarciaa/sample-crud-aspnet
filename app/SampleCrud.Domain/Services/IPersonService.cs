ec7b403491f29a6381175849a89aef3bc8ec2fed
guigarciaa <guigarcia.a@outlook.com>
2023-10-18 12:13:44 -0300

    fix GetById Id is required

--- app/SampleCrud.Domain/Services/IPersonService.cs
+++ app/SampleCrud.Domain/Services/IPersonService.cs
@@ -5,7 +5,7 @@ namespace SampleCrud.Domain.Services
     public interface IPersonService
     {
         Task<IEnumerable<Person>> GetPersons();
-        Task<Person?> GetById(Guid? id);
+        Task<Person?> GetById(Guid id);
         void Add(Person person);
         void Update(Person person);
         void Remove(Guid id);
