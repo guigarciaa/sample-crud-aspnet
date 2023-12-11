383fb434de8427c19c77278d0b5bdbc69cdeeb53
guigarciaa <guigarcia.a@outlook.com>
2023-12-10 23:51:24 -0300

    reducing testing time

--- tests/stress-testing/script.js
+++ tests/stress-testing/script.js
@@ -4,9 +4,9 @@ import { sleep } from "k6";
 export const options = {
   // Key configurations for Stress in this section
   stages: [
-    { duration: "2m", target: 200 }, // traffic ramp-up from 1 to a higher 200 users over 10 minutes.
-    { duration: "3m", target: 200 }, // stay at higher 200 users for 30 minutes
-    { duration: "2m", target: 0 }, // ramp-down to 0 users
+    { duration: "1m", target: 200 }, // traffic ramp-up from 1 to a higher 200 users over 10 minutes.
+    { duration: "1m", target: 200 }, // stay at higher 200 users for 30 minutes
+    { duration: "1m", target: 0 }, // ramp-down to 0 users
   ],
 };
 
