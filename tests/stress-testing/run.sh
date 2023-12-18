5abda53c214f8a44c0f338d769430c64f62bf50b
Guilherme Garcia <guigarcia.a@outlook.com>
2023-12-18 09:30:13 -0300

    Rename stress tests
    - Rename stress tests

--- tests/stress-testing/run.sh
+++ tests/stress-testing/run.sh
@@ -2,4 +2,4 @@ docker pull grafana/k6;
 
 # docker pull grafana/k6:master-with-browser;
 
-docker run --net=host --rm -i grafana/k6 run - <script.js;
+docker run --net=host --rm -i grafana/k6 run - <stress-script.js;
