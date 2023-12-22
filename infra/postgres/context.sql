1b2c6b8f607be65c0de88482bccbc83472c2973f
Guilherme Garcia <guigarcia.a@outlook.com>
2023-12-22 18:47:55 -0300

    Limitations and Network
    - Renaming docker-compose network for samplecrudnetwork
    - Linked k6 container in same network samplecrudnetwork
    - Increased 80 for 350 connections
    - Creating an index for table Person and creating a more efficient function for search fields.

--- infra/postgres/context.sql
+++ infra/postgres/context.sql
@@ -1,9 +1,23 @@
-CREATE TABLE "Person" (
-    "Id" uuid NOT NULL,
-    "Nickname" character varying(32) NOT NULL,
-    "Name" character varying(100) NOT NULL,
-    "Email" text NOT NULL,
-    "Birthday" date NOT NULL,
-    "Stack" text[] NOT NULL,
-    CONSTRAINT "PK_Person" PRIMARY KEY ("Id")
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE OR REPLACE FUNCTION generate_searchable(_name VARCHAR, _nickname VARCHAR, _stack JSON)
+    RETURNS TEXT AS $$
+    BEGIN
+    RETURN _name || _nickname || _stack;
+    END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE TABLE IF NOT EXISTS Person (
+    Id uuid DEFAULT UNIQUE NOT NULL,
+    Nickname TEXT UNIQUE NOT NULL,
+    Name TEXT NOT NULL,
+    Email TEXT NOT NULL,
+    Birthday DATE NOT NULL,
+    Stack JSON,
+    CONSTRAINT "PK_Person" PRIMARY KEY ("Id"),
+    searchable text GENERATED ALWAYS AS (generate_searchable(Name, Nickname, Stack)) STORED
 );
+
+CREATE INDEX IF NOT EXISTS idx_persons_searchable ON public.Person USING gist (searchable public.gist_trgm_ops (siglen='64'));
+
+CREATE UNIQUE INDEX IF NOT EXISTS persons_nickname_index ON public.Person USING btree (nickname);
--- tests/stress-testing/run.sh
+++ tests/stress-testing/run.sh
@@ -1,5 +1,6 @@
 docker pull grafana/k6;
 
-# docker pull grafana/k6:master-with-browser;
+docker run  --cpus=0.5 --memory=0.5 --net=infra_samplecrudnetwork \
+            --rm -i grafana/k6 run - <local-import-person-data.js;
 
-docker run --net=host --rm -i grafana/k6 run - <stress-script.js;
+# docker run --net=host -v ./../mass/:/usr/share/mass/ --rm -i grafana/k6 run - <local-import-person-data.js
