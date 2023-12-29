b0268bb7171c20dbc943909fa7e0de0daa9981d4
Guilherme Garcia <guigarcia.a@outlook.com>
2023-12-28 21:49:47 -0300

    feat: Adding more CPU for stress tests and adding some
    - Update CPU used for 1.5 in stress test script
    - Creating 2 metrics for monitor tests executions

--- tests/stress-testing/stress-script.js
+++ tests/stress-testing/stress-script.js
@@ -3,6 +3,10 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export const options = {
+  thresholds: {
+    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
+    http_req_duration: ["p(95)<200"], // 95% of requests should be below 200ms
+  },
   scenarios: {
     pre_warmup: {
       executor: "shared-iterations",
